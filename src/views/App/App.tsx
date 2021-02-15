ffd7d9e16d93611e158670659842c50b0195fd03
Hector Salinas <hector_sag@outlook.com>
2021-02-15 13:25:15 -0600

    Icons added

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import '../../styles/app.scss';
-import { Button, Card, Col, Container, Form, Row } from 'react-bootstrap';
+import {
+	Button,
+	Card,
+	Col,
+	Container,
+	Form,
+	InputGroup,
+	Row
+} from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
 import { searchWeatherAction } from '../../store/reducers/features/weather';
 import { useForm } from 'react-hook-form';
 import getBackground from '../../utils/background';
 import moment from 'moment';
+import {
+	WiDirectionDown,
+	WiDirectionUp,
+	WiRaindrop,
+	WiStrongWind,
+	WiThermometer,
+	WiThermometerExterior
+} from 'react-icons/wi';
 
 interface IFormData {
 	location: string;
@@ -81,8 +97,12 @@ function App() {
 										)}
 									/>
 									<Card.ImgOverlay className="text-right">
-										<Card.Title className="day">{moment().format("dddd DD")}</Card.Title>
-										<Card.Subtitle className="month-year">{moment().format("MMMM-YYYY")}</Card.Subtitle>
+										<Card.Title className="day">
+											{weather?.main ? moment().format('dddd DD') : ''}
+										</Card.Title>
+										<Card.Subtitle className="month-year">
+											{weather?.main ? moment().format('MMMM-YYYY') : ''}
+										</Card.Subtitle>
 									</Card.ImgOverlay>
 									<Card.Body>
 										<Row>
@@ -90,10 +110,10 @@ function App() {
 												<Card.Title>
 													{weather?.name
 														? `${weather.name}, ${weather.sys?.country}`
-														: 'W/I'}
+														: 'N / A'}
 												</Card.Title>
 												<Card.Subtitle>
-													{weather?.weather ? weather.weather[0].main : 'W/I'}
+													{weather?.weather ? weather.weather[0].main : 'N / A'}
 												</Card.Subtitle>
 											</Col>
 											<Col className="mt-2">
@@ -101,99 +121,141 @@ function App() {
 													<Col xs={6} xl={4}>
 														<Form.Group>
 															<Form.Label>Temp</Form.Label>
-															<Form.Control
-																plaintext
-																readOnly
-																disabled
-																type="text"
-																value={
-																	weather?.main
-																		? `${weather.main.temp} °C`
-																		: 'W/I'
-																}
-															/>
+															<InputGroup>
+																<InputGroup.Prepend>
+																	<InputGroup.Text>
+																		{<WiThermometerExterior />}
+																	</InputGroup.Text>
+																</InputGroup.Prepend>
+																<Form.Control
+																	plaintext
+																	readOnly
+																	disabled
+																	type="text"
+																	value={
+																		weather?.main
+																			? `${weather.main.temp} °C`
+																			: 'N / A'
+																	}
+																/>
+															</InputGroup>
 														</Form.Group>
 													</Col>
 													<Col xs={6} xl={4}>
 														<Form.Group>
 															<Form.Label>Feels Like</Form.Label>
-															<Form.Control
-																plaintext
-																readOnly
-																disabled
-																type="text"
-																value={
-																	weather?.main
-																		? `${weather.main.feels_like} °C`
-																		: 'W/I'
-																}
-															/>
+															<InputGroup>
+																<InputGroup.Prepend>
+																	<InputGroup.Text>
+																		{<WiThermometer />}
+																	</InputGroup.Text>
+																</InputGroup.Prepend>
+																<Form.Control
+																	plaintext
+																	readOnly
+																	disabled
+																	type="text"
+																	value={
+																		weather?.main
+																			? `${weather.main.feels_like} °C`
+																			: 'N / A'
+																	}
+																/>
+															</InputGroup>
 														</Form.Group>
 													</Col>
 													<Col xs={6} xl={4}>
 														<Form.Group>
 															<Form.Label>Min</Form.Label>
-															<Form.Control
-																plaintext
-																readOnly
-																disabled
-																type="text"
-																value={
-																	weather?.main
-																		? `${weather.main.temp_min} °C`
-																		: 'W/I'
-																}
-															/>
+															<InputGroup>
+																<InputGroup.Prepend>
+																	<InputGroup.Text>
+																		{<WiDirectionDown />}
+																	</InputGroup.Text>
+																</InputGroup.Prepend>
+																<Form.Control
+																	plaintext
+																	readOnly
+																	disabled
+																	type="text"
+																	value={
+																		weather?.main
+																			? `${weather.main.temp_min} °C`
+																			: 'N / A'
+																	}
+																/>
+															</InputGroup>
 														</Form.Group>
 													</Col>
 													<Col xs={6} xl={4}>
 														<Form.Group>
 															<Form.Label>Max</Form.Label>
-															<Form.Control
-																plaintext
-																readOnly
-																disabled
-																type="text"
-																value={
-																	weather?.main
-																		? `${weather.main.temp_max} °C`
-																		: 'W/I'
-																}
-															/>
+															<InputGroup>
+																<InputGroup.Prepend>
+																	<InputGroup.Text>
+																		{<WiDirectionUp />}
+																	</InputGroup.Text>
+																</InputGroup.Prepend>
+																<Form.Control
+																	plaintext
+																	readOnly
+																	disabled
+																	type="text"
+																	value={
+																		weather?.main
+																			? `${weather.main.temp_max} °C`
+																			: 'N / A'
+																	}
+																/>
+															</InputGroup>
 														</Form.Group>
 													</Col>
 													<Col xs={6} xl={4}>
 														<Form.Group>
 															<Form.Label>Wind</Form.Label>
-															<Form.Control
-																plaintext
-																readOnly
-																disabled
-																type="text"
-																value={
-																	weather?.wind
-																		? `${(weather.wind.speed / 0.44704).toFixed(
-																				2
-																		  )} mph`
-																		: 'W/I'
-																}
-															/>
+															<InputGroup>
+																<InputGroup.Prepend>
+																	<InputGroup.Text>
+																		{<WiStrongWind />}
+																	</InputGroup.Text>
+																</InputGroup.Prepend>
+																<Form.Control
+																	plaintext
+																	readOnly
+																	disabled
+																	type="text"
+																	value={
+																		weather?.wind
+																			? `${(
+																					weather.wind.speed / 0.44704
+																			  ).toFixed(2)} mph`
+																			: 'N / A'
+																	}
+																/>
+															</InputGroup>
 														</Form.Group>
 													</Col>
 													<Col xs={6} xl={4}>
 														<Form.Group>
 															<Form.Label>Humidity</Form.Label>
-															<Form.Control
-																plaintext
-																readOnly
-																disabled
-																type="text"
-																value={
-																	weather?.main
-																		? `${weather.main.humidity}%`
-																		: 'W/I'
-																}
-															/>
+															<InputGroup>
+																<InputGroup.Prepend>
+																	<InputGroup.Text>
+																		{<WiRaindrop />}
+																	</InputGroup.Text>
+																</InputGroup.Prepend>
+																<Form.Control
+																	plaintext
+																	readOnly
+																	disabled
+																	type="text"
+																	value={
+																		weather?.main
+																			? `${weather.main.humidity}%`
+																			: 'N / A'
+																	}
+																/>
+															</InputGroup>
 														</Form.Group>
 													</Col>
 												</Form.Row>
