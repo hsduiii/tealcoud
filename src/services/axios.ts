226c3767c6a6eb5074a83999bc6c0be235cb7a6e
Hector Salinas <hector_sag@outlook.com>
2021-04-15 10:02:56 -0500

    Save before fix API KEY

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const weatherAPI = axios.create({
-    baseURL: process.env.REACT_APP_WEATHER_API_URL,
+    baseURL: process.env.REACT_APP_WEATHER_API_URL || 'https://api.openweathermap.org/data/2.5',
   });
--- src/services/weather.ts
+++ src/services/weather.ts
@@ -1,16 +1,18 @@
 import IWeatherModel from '../models/weather';
 import { weatherAPI } from './axios';
 
+const apiKey = process.env.REACT_APP_WEATHER_API_KEY || '9b7ec3200065415fdb3761f9c7ed5e81';
+
 export async function getWeather(location: string): Promise<IWeatherModel> {
 	const res = await weatherAPI.get(
-		`/weather?q=${location}&units=metric&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`
+		`/weather?q=${location}&units=metric&APPID=${apiKey}`
 	);
 	return res.data;
 }
 
 export async function getWeatherUsingCoordinates(lat: number, lon: number): Promise<IWeatherModel> {
 	const res = await weatherAPI.get(
-		`/weather?lat=${lat}&lon=${lon}&units=metric&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`
+		`/weather?lat=${lat}&lon=${lon}&units=metric&APPID=${apiKey}`
 	);
 	return res.data;
 }
